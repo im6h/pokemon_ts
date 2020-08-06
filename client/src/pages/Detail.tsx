0ca40958c35c34e9420f91550e045e1fd3f615c0
tjjone98 <tzero1898@gmail.com>
2020-08-06 23:04:13 +0700

    fix[style]: test style

--- client/src/pages/Detail.tsx
+++ client/src/pages/Detail.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React from 'react';
+import '../styles/pages/detail.scss';
 
 function Detail() {
-  return <div>This is detail</div>;
+	return (
+		<div className="container">
+			<div className="detail__image">
+				<img
+					src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png"
+					alt=""
+				/>
+			</div>
+			<div className="detail__info">
+				<div>
+					<p>Dang Vh</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Detail;
